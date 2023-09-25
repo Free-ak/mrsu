6120b0d16ad3ee682e5bac558bd8c609ff635ac7
laptop sergey <sergzor@list.ru>
2023-09-26 00:27:18 +0300

    first commit

---
+++ app/src/main/java/com/example/mrsu/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mrsu
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_main)
+
+    }
+}
