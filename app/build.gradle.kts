d82eab50c3e15ade75958c8eb181be7ea71f14d5
udako <sergzor@list.ru>
2023-10-02 22:53:14 +0300

    day2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.mrsu"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mrsu"
@@ -44,7 +44,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("de.rhododendron:circumnavigate:3.1.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
